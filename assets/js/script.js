0c6fb718daeefe298f4c53d74c2c457e5957de71
sdott3 <sdotthomas347@gmail.com>
2022-06-05 05:34:21 -0400

    document function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,51 @@
-// display current date
-$("currentDay").text(moment().format("MM-DD-YYYY"));
+$(document).ready(function() {
+    // listen for save button clicks
+    $(".saveBtn").on("click", function() {
+      // get nearby values
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+  
+      // save in localStorage
+      localStorage.setItem(time, value);
+    });
+  
+    function hourUpdater() {
+      // get current number of hours
+      var currentHour = moment().hours();
+  
+      // loop over time blocks
+      $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+        // check if we've moved past this time
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } 
+        else if (blockHour === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
 
+    hourUpdater();
+    
+    // set up interval to check if current time needs to be updated
 
+    // saved data from localStorage
+    $("hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("hour-2 .description").val(localStorage.getItem("hour-2"));
+    $("hour-3 .description").val(localStorage.getItem("hour-3"));
+    $("hour-4 .description").val(localStorage.getItem("hour-4"));
+    $("hour-5 .description").val(localStorage.getItem("hour-5"));
 
-// saved data from localStorage
-$("hour-9.description").val(localStorage.getItem("hour-9"));
-$("hour-10.description").val(localStorage.getItem("hour-10"));
-$("hour-11.description").val(localStorage.getItem("hour-11"));
-$("hour-12.description").val(localStorage.getItem("hour-12"));
-$("hour-1.description").val(localStorage.getItem("hour-1"));
-$("hour-2.description").val(localStorage.getItem("hour-2"));
-$("hour-3.description").val(localStorage.getItem("hour-3"));
-$("hour-4.description").val(localStorage.getItem("hour-4"));
-$("hour-5.description").val(localStorage.getItem("hour-5"));
+});
